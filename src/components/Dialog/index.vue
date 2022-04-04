acb50c7eae59defebbb50328f4a89c4de41e1798
周爱桩 <840043348@qq.com>
2022-04-04 11:31:25 +0800

    插槽全家桶

---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header class="bgff5650" style="height:100px;">
+      <slot name="header"></slot>
+    </header>
+    <main class="bg1890ff" style="height:200px;">
+      <div v-for="(item, index) in data" :key="item">
+        <slot :index="index" :data="item"></slot> <!--匿名插槽--> <!--插槽传参-->
+      </div>
+    </main>
+    <footer class="bgc7a35d" style="height:100px;">
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from "vue"
+type names = {
+  name: string,
+  age: number
+}
+const data = reactive<names[]>([{
+  name: '小满插槽',
+  age:20
+}])
+</script>
+<style scoped>
+</style>
--- src/layout/Content/index.vue
+++ src/layout/Content/index.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="f1 ml20 mr20 mt20 mb20 ovya" style="border:1px solid #ccc;">
-    <div class="pt20 pl20 pr20 pb20 bdbc" v-for="item in 100" :key="item">
+    <!-- <div class="pt20 pl20 pr20 pb20 bdbc" v-for="item in 100" :key="item">
       <Card :content="`我是第${item}个`"></Card>
-    </div>
+    </div> -->
+    <Dialog>
+      <template #header> <!--<template #[name]>--> <!--<template v-slot="header">-->
+        <div>我是header</div>
+      </template>
+      <template #default="row"> <!--{data, index}-->
+        <div>{{row.data}}----{{row.index}}</div>
+      </template>
+      <template #footer> <!--v-slot:footer-->
+        <div>我是footer</div>
+      </template>
+    </Dialog>
   </div>
 </template>
 <script setup lang="ts">
+import Dialog from "../../components/Dialog/index.vue"
+import { ref } from "vue"
+// 动态插槽
+let name = ref('footer')
 
 </script>
 <style scoped>
