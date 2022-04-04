d8f9e7bf1448465bb5e424c0c299e580cf865247
周爱桩 <840043348@qq.com>
2022-04-04 17:54:37 +0800

    增加自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <layout></layout>
   <B></B>
+  <div>{{message}}</div>
 </template>
 <script setup lang="ts">
 import layout from "./layout/index.vue"
 import B from "./components/B/index.vue"
 import { provide } from "vue"
 provide('flag', false)
+let message = ref<string>('asdasd===')
 </script>
 <style lang='less'>
 
