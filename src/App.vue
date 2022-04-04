a87939d227e01fedfeef817bb6f9e05ab33b5b42
周爱桩 <840043348@qq.com>
2022-04-04 17:45:19 +0800

    曾加provide inject

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <layout></layout>
+  <B></B>
 </template>
 <script setup lang="ts">
 import layout from "./layout/index.vue"
+import B from "./components/B/index.vue"
+import { provide } from "vue"
+provide('flag', false)
 </script>
 <style lang='less'>
 
---
+++ src/components/B/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    {{data}}
+  </div>
+</template>
+<script setup lang="ts">
+import {inject} from "vue"
+let data = inject('flag')
+console.log(data)
+</script>
+<style scoped>
+</style>
