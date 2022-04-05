1f6055633a91c174b77a6a8fb25cb62943f7df41
周爱桩 <840043348@qq.com>
2022-04-05 16:44:15 +0800

    递归组件

--- src/router/modules/component.ts
+++ src/router/modules/component.ts
@@ -2,4 +2,5 @@ import { RouteRecordRaw } from "vue-router"
 export const cmpt:Array<RouteRecordRaw> = [
   { path: '/useCom', component: () => import('../../views/studyCom/useCom/index.vue') },
   { path: '/definePropsEmit', component: () => import('../../views/studyCom/definePropsEmit/father.vue') },
+  { path: '/recursionCom', component: () => import('../../views/studyCom/recursionCom/index.vue') },
 ]
