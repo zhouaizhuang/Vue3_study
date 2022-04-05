2e117b7ab5d1911a1f326e85c729af11ead1f64e
周爱桩 <840043348@qq.com>
2022-04-05 17:17:44 +0800

    新增插槽

--- src/router/modules/component.ts
+++ src/router/modules/component.ts
@@ -4,4 +4,5 @@ export const cmpt:Array<RouteRecordRaw> = [
   { path: '/definePropsEmit', component: () => import('../../views/studyCom/definePropsEmit/father.vue') },
   { path: '/recursionCom', component: () => import('../../views/studyCom/recursionCom/index.vue') },
   { path: '/dynamicCom', component: () => import('../../views/studyCom/dynamicCom/index.vue') },
+  { path: '/slot', component: () => import('../../views/studyCom/slot/index.vue') },
 ]
