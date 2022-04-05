4b94afd9fee3ee4661d9803015fed74d207bd810
周爱桩 <840043348@qq.com>
2022-04-05 17:10:48 +0800

    动态组件切换

--- src/router/modules/component.ts
+++ src/router/modules/component.ts
@@ -3,4 +3,5 @@ export const cmpt:Array<RouteRecordRaw> = [
   { path: '/useCom', component: () => import('../../views/studyCom/useCom/index.vue') },
   { path: '/definePropsEmit', component: () => import('../../views/studyCom/definePropsEmit/father.vue') },
   { path: '/recursionCom', component: () => import('../../views/studyCom/recursionCom/index.vue') },
+  { path: '/dynamicCom', component: () => import('../../views/studyCom/dynamicCom/index.vue') },
 ]
---
+++ src/views/studyCom/dynamicCom/B.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt30">
+    <div class="pt30 pb30 pr30 pl30 bdc">我是B组件</div>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+</style>
