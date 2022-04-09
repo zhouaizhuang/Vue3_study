40cb4040f87b34c61298fbb8c70e8b60a4df9f68
周爱桩 <840043348@qq.com>
2022-04-09 09:02:42 +0800

    首次渲染动画

--- src/router/modules/transition.ts
+++ src/router/modules/transition.ts
@@ -2,4 +2,5 @@ import { RouteRecordRaw } from "vue-router"
 export const transition:Array<RouteRecordRaw> = [
   { path: '/transition', component: () => import('../../views/animate/testTransition/index.vue') },
   { path: '/transition2', component: () => import('../../views/animate/testTransition2/index.vue') },
+  { path: '/transition3', component: () => import('../../views/animate/testTransition3/index.vue') },
 ]
---
+++ src/views/animate/testTransition3/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mh100vh">
+    <transition
+      appear
+      appear-from-class="from"
+      appear-active-class="active"
+      appear-to-class="to"      
+    >
+      <div class="box"></div>
+    </transition>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.box{
+  width:200px;
+  height:200px;
+  background:red;
+}
+/**入场动画 */
+.from{
+  width:0;
+  height:0;
+}
+.active{
+  transition: all 1.5s ease;
+}
+.to{
+  width:200px;
+  height:200px;
+}
+/**出场动画 */
+.fade-leave-from{
+  width:200px;
+  height:200px;
+  transform: rotate(360deg);
+}
+.fade-leave-active{
+  transition: all .5s ease;
+}
+.fade-leave-to{
+  width:0;
+  height:0;
+}
+</style>
