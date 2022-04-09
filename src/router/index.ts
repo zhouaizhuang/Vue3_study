058d886eb43a545bfbae53bf0a34ca3416713ee2
周爱桩 <840043348@qq.com>
2022-04-09 09:51:59 +0800

    删除不需要的文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,6 @@ import { transition } from "./modules/transition"
 const routes:Array<RouteRecordRaw> = [
   { path: '/', redirect: { name: 'index' } },
   { path: '/index', name: 'index', component: () => import('../views/index.vue') },
-  { path: '/regist', component: () => import('../components/regist/index.vue') },
   ...refs,
   ...reactive,
   ...monitor,
