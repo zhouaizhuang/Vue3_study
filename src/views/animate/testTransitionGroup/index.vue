86a7d69f4c8553771b4cccec33facf11772429ae
周爱桩 <840043348@qq.com>
2022-04-09 09:27:00 +0800

    删除冗余代码

--- src/views/animate/testTransitionGroup/index.vue
+++ src/views/animate/testTransitionGroup/index.vue
@@ -24,34 +24,4 @@ const del = () => {
 </script>
 <style src="../animate.css"></style>
 <style scoped>
-.box{
-  width:200px;
-  height:200px;
-  background:red;
-}
-/**入场动画 */
-.fade-enter-from{
-  width:0;
-  height:0;
-}
-.fade-enter-active{
-  transition: all 1.5s ease;
-}
-.fade-enter-to{
-  width:200px;
-  height:200px;
-}
-/**出场动画 */
-.fade-leave-from{
-  width:200px;
-  height:200px;
-  transform: rotate(360deg);
-}
-.fade-leave-active{
-  transition: all .5s ease;
-}
-.fade-leave-to{
-  width:0;
-  height:0;
-}
 </style>
