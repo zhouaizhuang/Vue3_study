34425ef056b94aeb26c100f73b793f280213931f
周爱桩 <840043348@qq.com>
2022-09-09 00:38:18 +0800

    新增首页

---
+++ src/views/index/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import {add} from "./index.js"
+  import {ref} from "vue"
+  const text = ref(1)
+  setTimeout(() => {
+    text.value++
+  }, 1000)
+  const sum = add(1,2)
+</script>
+<template src="./index.html"></template>
+<style scoped></style>
